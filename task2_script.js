89b6b895567fd8828ee87a13245867462347f28a
ArjunVenkitraman <ramukpully@gmail.com>
2023-02-25 14:29:41 +0530

    task2completed

--- task2_script.js
+++ task2_script.js
@@ -68,36 +68,19 @@ function forof() {
     }
     document.getElementsByName("changes")[0].value = str;
 }
-var date = new Date();
-function date(){
-        var date = new Date();
-        var hours = date.getHours();
-        var min = date.getMinutes();
-        var sec = date.getSeconds();
-        var period = "AM";
+function date() {
+    var date = new Date();
+    var day = date.getDate();
+    var month = date.getMonth() + 1;
+    var year = date.getFullYear();
+    document.getElementsByName("changes")[0].value = (day + "/" + month + "/" + year);
 
-        console.log(printtime);
-        if (hours == 0) {
-            hours = 12;
-        }
-        if (hours > 12) {
-            hours = hours - 12;
-            period = "PM";
-        }
-        if (hours < 10) {
-            hours = "0" + hours;
-        }
-        if (min < 10) {
-            min = "0" + min;
-        }
-        if (sec < 10) {
-            sec = "0" + sec;
-        }
-        var time = hours + ":" + min + ":" + sec + "-" + period;
-        document.getElementsByName("changes")[0].value=time;
-        setTimeout(printtime, 1000);
-    }
-    printtime();
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    document.getElementsByName("changes")[0].value = (day + "/" + month + "/" + year + " Time - " + h + ":" + m + ":" + s);
+}
 
 
 function validate() {
@@ -126,26 +109,61 @@ function validate() {
     else if (email == "") {
         alert("Email should not be empty");
     }
-    else if (!filter.test(email)){
+    else if (!filter.test(email)) {
         alert("Please follow email format");
     }
-    else if(password==""){
+    else if (password == "") {
         alert("Password should not be empty")
     }
-    else if(!pwd_expression.test(password)){
+    else if (!pwd_expression.test(password)) {
         alert("Password must contain special letters,alphabets,numbers")
     }
-    else if(password.length<8 || password.length>12){
+    else if (password.length < 8 || password.length > 12) {
         alert("Password must contain words in between 8 and 12 ");
     }
-    else if(password != cpassword){
+    else if (password != cpassword) {
         alert("Password must be same");
     }
-    else{
+    else {
         alert("Registration successfully completed");
-        window.open("https://www.google.co.in/");
-        //window.location.href="https://www.google.co.in/";
+        //window.open("https://www.google.co.in/");
+        // window.location.href="D:\javascritp\details.html";
+        window.location = "http://127.0.0.1:5500/task2-q2b.html";
     }
- }
+}
+
+
+var pattern = /^([0-9]{4})-([0-9]{2})-([0-9]{2})$/;
+
+function detail() {
+    var fname = document.form2.Fname.value;
+    var lname = document.form2.Lname.value;
+    var dob = document.form2.Dob.value;
+    var gender = document.form2.radio.value;
+
+    if (fname == "" || lname=="" || dob==""||gender=="") {
+        alert("Fname should not be empty");
+    }
+
+    else if (!pattern.test(dob)) {
+        console.log(dob);
+        alert("Follow corrrect date of birth format");
+    }
+
+    var d1 = new Date(dob);
+    var d2 = new Date();
+   
+    var diff = d2.getTime() - d1.getTime();
+
+    var daydiff = diff / (1000 * 60 * 60 * 24);
+    //document.write(" Total number of days between <b> " + d1 + " </b> and <b> " + d2 + " </b> is: <b> " + daydiff + " days </b>");
+    if(diff>=18){
+        document.getElementById("success").innerHTML=fname+" is eligible to vote";
+    }
+    else{
+        document.getElementById("success").innerHTML=fname+" , wait to reach 18";
+
+    }
+}
 
 
